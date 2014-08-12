b6a5d66ba45e5384098ee67649acc67d08f983db
Michael Schmatz <michaelschmatz@gmail.com>
2014-08-12 11:30:01 -0700

    Reorganized textview and added uiview experiment

--- CodeCombat/EditorTextView.swift
+++ CodeCombat/EditorTextView.swift
@@ -17,43 +17,82 @@ class EditorTextView: UITextView {
   var currentHighlightingView:UIView? = nil
   
   override func drawRect(rect: CGRect) {
-    
     if shouldShowLineNumbers {
+      drawLineNumberBackground()
       drawLineNumbers()
-      
     }
+    drawClickableBoxesOnHello()
     super.drawRect(rect)
+  }
+  
+  private func drawClickableBoxesOnHello() {
+    let helloExpression = NSRegularExpression(
+    pattern: "hello",
+    options: nil,
+    error: nil)
+    let stringToDrawUpon = textStorage.string
+    let range = NSMakeRange(0, stringToDrawUpon.utf16Count)
+    let matches = helloExpression.matchesInString(stringToDrawUpon,
+      options: nil,
+      range: range)
+    for regexMatch in matches {
+      let match = regexMatch as NSTextCheckingResult
+      let start = positionFromPosition(beginningOfDocument, offset: match.range.location)
+      let end =  positionFromPosition(start, offset: match.range.length)
+      let textRange = textRangeFromPosition(start, toPosition: end)
+      
+      let resultRect = firstRectForRange(textRange)
+      let highlightView = UIView(frame: resultRect)
+      highlightView.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 0.5)
+      addSubview(highlightView)
+    }
   }
   
   private func drawLineNumbers() {
     let Storage = textStorage as EditorTextStorage
     let Context = UIGraphicsGetCurrentContext()
     let Bounds = bounds
-    //234	219	169
-    let LineNumberBackgroundColor = UIColor(red: CGFloat(234.0/256.0), green: CGFloat(219.0/256.0), blue: CGFloat(169.0/256.0), alpha: 1)
-    CGContextSetFillColorWithColor(Context, LineNumberBackgroundColor.CGColor)
-    let LineNumberBackgroundRect = CGRectMake(Bounds.origin.x, Bounds.origin.y, lineNumberWidth, Bounds.size.height)
-    CGContextFillRect(Context, LineNumberBackgroundRect)
     
-    let textRange = layoutManager.glyphRangeForBoundingRect(Bounds, inTextContainer: textContainer)
-    let GlyphsToShow = layoutManager.glyphRangeForCharacterRange(textRange, actualCharacterRange: nil)
+    let textRange = layoutManager.glyphRangeForBoundingRect(Bounds,
+      inTextContainer: textContainer)
+    let GlyphsToShow = layoutManager.glyphRangeForCharacterRange(textRange,
+      actualCharacterRange: nil)
     var lineNumber = 0
     let FirstLine = 0 //compute this somehow
     var visibleLines = 0
-    layoutManager.enumerateLineFragmentsForGlyphRange(GlyphsToShow,
-      usingBlock: { [weak self] aRect, aUsedRect, textContainer, glyphRange, stop in
-        let CharacterRange = self!.layoutManager.characterRangeForGlyphRange(glyphRange, actualGlyphRange: nil)
+    func lineFragmentClosure(aRect:CGRect, aUsedRect:CGRect,
+      textContainer:NSTextContainer!, glyphRange:NSRange,
+      stop:UnsafeMutablePointer<ObjCBool>) -> Void {
+        let CharacterRange = layoutManager.characterRangeForGlyphRange(glyphRange, actualGlyphRange: nil)
         let ParagraphRange = Storage.string()!.paragraphRangeForRange(CharacterRange)
         //To avoid drawing numbers on wrapped lines
         if NSEqualRanges(CharacterRange, ParagraphRange) {
           visibleLines++
           lineNumber++
           let LineNumberString = NSString(string: "\(lineNumber)")
-          let Size = LineNumberString.sizeWithAttributes(self!.textAttributes)
-          let Point = CGPointMake(self!.lineNumberWidth - 4 - Size.width, aRect.origin.y + 8)
-          LineNumberString.drawAtPoint(Point, withAttributes: self!.textAttributes)
+          let Size = LineNumberString.sizeWithAttributes(textAttributes)
+          let Point = CGPointMake(lineNumberWidth - 4 - Size.width, aRect.origin.y + 8)
+          LineNumberString.drawAtPoint(Point, withAttributes: textAttributes)
         }
-    })
+    }
+    layoutManager.enumerateLineFragmentsForGlyphRange(GlyphsToShow,
+      usingBlock: lineFragmentClosure)
+  }
+  
+  private func drawLineNumberBackground() {
+    let context = UIGraphicsGetCurrentContext()
+    let LineNumberBackgroundColor = UIColor(
+      red: CGFloat(234.0/256.0),
+      green: CGFloat(219.0/256.0),
+      blue: CGFloat(169.0/256.0),
+      alpha: 1)
+    CGContextSetFillColorWithColor(context, LineNumberBackgroundColor.CGColor)
+    let LineNumberBackgroundRect = CGRect(
+      x: bounds.origin.x,
+      y: bounds.origin.y,
+      width: lineNumberWidth,
+      height: bounds.size.height)
+    CGContextFillRect(context, LineNumberBackgroundRect)
   }
   
   func showLineNumbers() {
@@ -61,8 +100,9 @@ class EditorTextView: UITextView {
       return
     }
     font = UIFont(name: "Courier", size: 20)
-    contentSize = CGSizeMake(bounds.size.width - lineNumberWidth, bounds.size.height)
-    //contentInset = UIEdgeInsetsMake(0, 0, 0, lineNumberWidth * 5)
+    contentSize = CGSize(
+      width: bounds.size.width - lineNumberWidth,
+      height: bounds.size.height)
     shouldShowLineNumbers = true
     resizeLineNumberGutter()
   }
@@ -78,7 +118,11 @@ class EditorTextView: UITextView {
       let Size = TotalLinesString.sizeWithAttributes([NSFontAttributeName: font])
       let ContainerRect = bounds
       let GutterPadding = CGFloat(5.0)
-      let Rect = CGRectMake(ContainerRect.origin.x, ContainerRect.origin.y, Size.width + GutterPadding, CGFloat.max)
+      let Rect = CGRect(
+        x: ContainerRect.origin.x,
+        y: ContainerRect.origin.y,
+        width: Size.width + GutterPadding,
+        height: CGFloat.max)
       lineNumberWidth = Rect.size.width
       textContainer.exclusionPaths = [UIBezierPath(rect: Rect)]
       numberOfCharactersInLineNumberGutter = NumberOfCharacters
@@ -94,10 +138,12 @@ class EditorTextView: UITextView {
   func handleItemPropertyDragEndedAtLocation(location:CGPoint, code:String) {
     currentHighlightingView?.removeFromSuperview()
     currentHighlightingView = nil
-    let GlyphIndex = layoutManager.glyphIndexForPoint(CGPointMake(0, location.y), inTextContainer: textContainer)
-    println(GlyphIndex)
+    let dragPoint = CGPoint(x: 0, y: location.y)
+    let GlyphIndex = layoutManager.glyphIndexForPoint(dragPoint,
+      inTextContainer: textContainer)
     textStorage.beginEditing()
-    textStorage.insertAttributedString(NSAttributedString(string: code), atIndex: GlyphIndex)
+    textStorage.insertAttributedString(NSAttributedString(string: code),
+      atIndex: GlyphIndex)
     textStorage.endEditing()
     setNeedsDisplay()
     
@@ -110,16 +156,28 @@ class EditorTextView: UITextView {
   }
   func getLineNumberRect(lineNumber:Int) -> CGRect{
     let LineHeight = CGFloat(font.lineHeight)
-    let LineNumberRect = CGRectMake(0, LineHeight * CGFloat(lineNumber), frame.width, LineHeight)
+    let LineNumberRect = CGRect(
+      x: 0,
+      y: LineHeight * CGFloat(lineNumber),
+      width: frame.width,
+      height: LineHeight)
     return LineNumberRect
   }
   
   func highlightLines(#startingLineNumber:Int, numberOfLines:Int) {
     let FirstLineNumberRect = getLineNumberRect(startingLineNumber)
-    let HighlightingRect = CGRectMake(FirstLineNumberRect.origin.x, FirstLineNumberRect.origin.y , FirstLineNumberRect.width, FirstLineNumberRect.height * CGFloat(numberOfLines))
+    let HighlightingRect = CGRect(
+      x: FirstLineNumberRect.origin.x,
+      y: FirstLineNumberRect.origin.y,
+      width: FirstLineNumberRect.width,
+      height: FirstLineNumberRect.height * CGFloat(numberOfLines))
     if currentHighlightingView == nil {
       currentHighlightingView = UIView(frame: HighlightingRect)
-      currentHighlightingView?.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 0.2)
+      currentHighlightingView?.backgroundColor = UIColor(
+        red: 1,
+        green: 0,
+        blue: 0,
+        alpha: 0.2)
       addSubview(currentHighlightingView!)
     }
     currentHighlightingView?.frame = HighlightingRect
