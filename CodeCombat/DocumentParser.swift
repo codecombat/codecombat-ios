a4fcceecc7d55614c177b666dddc3386cb49ce95
Michael Schmatz <michaelschmatz@gmail.com>
2014-08-29 11:42:21 -0400

    Simplified match code to use onigResult

--- CodeCombat/DocumentParser.swift
+++ CodeCombat/DocumentParser.swift
@@ -22,40 +22,12 @@ class Regex {
     return "\(regex.expression())  // \(lastIndex), \(lastFound)"
   }
   
-  func find(data:NSString, pos:Int) -> MatchObject {
+  func find(data:NSString, pos:Int) -> OnigResult? {
     if lastIndex > pos {
       lastFound = 0
     }
     lastIndex = pos
-    while lastFound < data.length {
-      let result = regex.search(data, start: Int32(lastFound))
-      //find(b []byte, n int, offset int) https://github.com/moovweb/rubex/blob/go1/regex.go#L169
-      //ret := r.re.FindStringSubmatchIndex(data[r.lastFound:])
-      //match = re.find(b, len(b), 0)
-      if result == nil {
-        break
-        //i have no idea what the rubex ret[0] means
-        //the match is beginning, end
-        
-      } else if result.rangeAt(0).location < pos {
-        let wtfrubex = result.rangeAt(0).location
-        if wtfrubex == 0 {
-          lastFound++
-        } else {
-          lastFound += wtfrubex
-        }
-        continue
-      }
-      var matchObj:MatchObject = []
-      for var i:UInt=0; i < result.count(); i++ {
-        matchObj.append(result.rangeAt(i).location)
-      }
-      //missing fix object here, probably nonfunctional
-      return matchObj
-    }
-    return []
-  }
-  
+    return regex.search(data, start: Int32(lastFound))
 }
 
 //The language provider is responsible for parsing files into languages
