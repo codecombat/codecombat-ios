cff62491c742a72fc637a81316ce1855c0cc593e
Michael Schmatz <michaelschmatz@gmail.com>
2014-09-13 16:27:28 -0400

    Removed documentHighlighter

--- CodeCombatTests/DocumentHighlighterTests.swift
+++ CodeCombatTests/DocumentHighlighterTests.swift
@@ -11,12 +11,10 @@ import XCTest
 
 class DocumentHighlighterTests: XCTestCase {
   
-  var documentHighlighter = DocumentHighlighter()
   let provider = LanguageProvider()
   override func setUp() {
     super.setUp()
     // Put setup code here. This method is called before the invocation of each test method in the class.
-    documentHighlighter = DocumentHighlighter()
   }
   
   override func tearDown() {
