0d673bd20ab853d2323c258c9f369d1b2a0021b1
Michael Schmatz <michaelschmatz@gmail.com>
2014-09-20 11:34:28 -0400

    Added picker selection method

--- CodeCombat/LanguageSelectionViewController.swift
+++ CodeCombat/LanguageSelectionViewController.swift
@@ -36,4 +36,8 @@ class LanguageSelectionViewController: UIViewController, UIPickerViewDataSource,
   func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
     return languages[row]
   }
+  
+  func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+    println("Selected language \(languages[row])")
+  }
 }
