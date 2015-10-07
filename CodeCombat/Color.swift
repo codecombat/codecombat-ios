50d6ea9690af31d7e92e9eb6b4c781572e9c76e0
Sam Soffes <sam@soff.es>
2015-10-07 13:50:02 -0700

    Clean up colors

---
+++ CodeCombat/Color.swift
@@ -0,0 +1,16 @@
+//
+//  Color.swift
+//  iPadClient
+//
+//  Created by Michael Schmatz on 7/27/14.
+//  Copyright (c) 2014 CodeCombat. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+	static let lightBrown = UIColor(red: 229 / 255, green: 208 / 255, blue: 136 / 255, alpha: 1)
+	static let gutterBorder = UIColor(red: 180 / 255,green: 166 / 255,blue: 146 / 255, alpha: 1)
+	static let darkBrown = UIColor(red: 80 / 255, green: 64 / 255, blue: 50 / 255, alpha: 1)
+	static let grassGreen = UIColor(red: 146 / 255, green: 202 / 255, blue: 54 / 255, alpha: 1)
+}
