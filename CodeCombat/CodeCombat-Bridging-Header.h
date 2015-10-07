6869426075a3819551c9f382f7508a28addc5d55
Sam Soffes <sam@soff.es>
2015-10-07 13:47:42 -0700

    Remove unused code

--- CodeCombat/CodeCombat-Bridging-Header.h
+++ CodeCombat/CodeCombat-Bridging-Header.h
@@ -1,10 +1,7 @@
-  //
+//
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
 #import "NSAttributedStringMarkdownParser.h"
 #import "OnigRegexp.h"
-#import "ActionSheetStringPicker.h"
-#import "AbstractActionSheetPicker.h"
-#import "SWActionSheet.h"
 #import "Reachability.h"
